cfe96b4379bb18912bc5024dcfaf531049544c92
LaughingKanta <cgsc1016@mail4.doshisha.ac.jp>
2021-09-19 02:00:50 +0900

    change parameter

--- Simulation/LPM/Robot_LPM＋CF.py
+++ Simulation/LPM/Robot_LPM＋CF.py
@@ -24,8 +24,8 @@ def Initialize():
     Amplitude = 32767. / 2.   #pulse Amplitude: 
     Fi = 80000    #: initial frequency [Hz]
     Ft = 40000  #: terminal frequency [Hz]
-    Duration =0.008 #LPM_duration [s]
-    Duration_CF =0.002 # CF_duration [s]
+    Duration =0.0075 #LPM_duration [s]
+    Duration_CF =0.0025 # CF_duration [s]
     Duration_mimic=0.01#mimicFM_duration[s]
     BatcallConstant=0.005#パラメータ
     draw_tx_spc_flg=True
